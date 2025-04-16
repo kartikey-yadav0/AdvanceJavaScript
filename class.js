37f3712478bff8a83f8bf7a7b000d042ead88739
Kartikey Yadav <kartikeyyadav11a@gmail.com>
2025-04-16 11:31:46 +0530

    class

---
+++ class.js
@@ -0,0 +1,26 @@
+// class ClassName{
+//     constructor(prop1,prop2){
+//         this.prop1=prop1;
+//     }
+// }
+// let obj = new ClassName('argg1','argg2');
+// console.log(obj.prop1)
+// console.log(obj.prop2)
+// class Dog{
+//     constructor(dogName,weight,color,breed){
+//         this.dogName=dogName;
+//         this.weight=weight;
+//         this.color=color;
+//         this.breed=breed;
+//     }
+// }
+// class person{
+//     constructor(firstname,lastname)
+//     {
+//         this.firstname=firstname;
+//         this.lastname=lastname;
+//     }
+// }
+// let p=new person("kartikey","yadav")
+// console.log(p.firstname)git
+// console.log(p.lastname)
---
+++ script.js
@@ -0,0 +1,75 @@
+// function sayHello()
+// {
+//     let you=prompt("what's your name?");
+//     console.log("hello",you+"I")
+// }
+// sayHello();
+
+
+// let varContainingFunction=function(){
+//     let varInFunction="I'm in a function.";
+//     console.log("hi there ",varInFunction); 
+// };
+// varContainingFunction();
+
+
+// let TextDecodeStream(para1,para2){
+//     return para1+" "+para2;
+// }
+// const arg1="argument1";
+// const arg2="argument2";
+// tester(arg1,arg2);
+
+// function addNumbers(num1, num2) {
+//    return parseInt(num1) + parseInt(num2);
+// }
+
+// const result = addNumbers("10", "20");
+// console.log(result);
+// function addNumbers(num1,num2){
+    // return parseInt(num1)+parseInt(num2);
+// }
+// const result=addNumbers("10","20");
+// console.log(result);
+// const doingArrowstuff = x => consol.log(x);
+// doingArrowstuff("hellow word");
+// let addTwoNumber = (x,y) => console.log(x=y):
+// const arr =["apple","orange","banana"];
+// arr.forEach(e=> console.log(e))://  
+// function addTwonumber(x+y){
+    // console.log(x+y);    
+// }
+// let arr = [5,9];
+// addTwoNumders(...sprad);
+// function addTwoNumber
+// #REST OPERATER 
+// function someFunction(param1+param2){
+//     console.log(param1,parm2);
+// }
+// someFunction('hi","there","how are you");
+// let resultArr = [];
+// for (let i = 0;i<10;i++){
+//     let result = addTwoNumder(i,2*i);
+//     resultsArr.push(result);
+// }
+// console.log(resultsArr);
+             // / returing with arrow function///
+//  addTwo Numbers =(x,y)=>{
+//     console.log("adding..");
+//     result x+y;
+// }let
+// function testAvailability(x){
+//     console.log(Available Headers:,x);
+// }
+// testAvailability("hi");
+// console.log("not avilable here":,x);
+
+// function testAvailability(x){
+//     let y = "local variable";
+//     console.log(Available here:"",x);
+// }
+// testAvailability("hi");
+// console.log("not avilable here:",y);
+ ///GLOBAL VARIABLE///
+// let globleVar ="Accesiableeverywhere!";
+// console.log("outerside function:",globleVar);
